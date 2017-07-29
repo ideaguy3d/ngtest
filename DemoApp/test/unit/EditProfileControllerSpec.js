4c880e2b8a23ad9d8fea77cd0fef2e39f9ecb224
Julius Alvarado <angularjs.uiux@gmail.com>
2017-07-28 23:44:02 -0700

    wrote an other test for a ctrl

--- DemoApp/test/unit/EditProfileControllerSpec.js
+++ DemoApp/test/unit/EditProfileControllerSpec.js
@@ -1,5 +1,27 @@
 'use strict';
 
-describe('EditProfileControllerSpec', function() {
+describe('EditProfileControllerSpec', function () {
+    var $ctrlConstr;
+    var scope;
+    var mockGravatarUrlBuilder;
 
+    beforeEach(module('eventsApp'));
+    beforeEach(inject(function ($controller, $rootScope) {
+        $ctrlConstr = $controller;
+        scope = $rootScope.$new();
+        mockGravatarUrlBuilder = sinon.stub({
+            'buildGravatarUrl': function () {}
+        });
+    }));
+
+    it('should build the gravatar with the given email address', function () {
+        $ctrlConstr('EditProfileController', {
+            '$scope': scope,
+            'gravatarUrlBuilder': mockGravatarUrlBuilder
+        });
+        var email = "javascript.uiux@gmail.com";
+        scope.getGravatarUrl(email);
+        expect(mockGravatarUrlBuilder.buildGravatarUrl.calledWith(email))
+            .toBe(true);
+    });
 });
